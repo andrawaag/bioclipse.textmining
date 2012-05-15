59d7bb080fa330a3e4548e0550609d55e389748c
Andra Waagmeester <andra.waagmeester@gmail.com>
2012-05-15 12:44:03 +0200

    clean indentation

--- plugins/net.bioclipse.pubmed/src/net/bioclipse/pubmed/business/IPubmedManager.java
+++ plugins/net.bioclipse.pubmed/src/net/bioclipse/pubmed/business/IPubmedManager.java
@@ -24,90 +24,90 @@ import net.bioclipse.core.business.BioclipseException;
 import net.bioclipse.managers.business.IBioclipseManager;
 
 @PublishedClass(
-    value="Manager for calling Pubmed through Eutils: "
+		value="Manager for calling Pubmed through Eutils: "
 )
 public interface IPubmedManager extends IBioclipseManager {
 
-	  @Recorded
-	  @PublishedMethod(
-	        params = "int pmid", 
-	        methodSummary = "Loads the PubMed entry from a given pmid as XML"
-	        	
-	    )
+	@Recorded
+	@PublishedMethod(
+			params = "int pmid", 
+			methodSummary = "Loads the PubMed entry from a given pmid as XML"
+
+	)
 	public String getPubMedEntry(int pmid)
-    throws IOException, BioclipseException, CoreException;
-	  
-	  
-	  @Recorded
-	  @PublishedMethod(
-	        params = "int pmid", 
-	        methodSummary = "Get full text link if available"
-	        	
-	    )
-	public String getFullTextLink(int pmid)
-    throws IOException, BioclipseException, CoreException;
-	  
-	  @Recorded
-	  @PublishedMethod(
-	        params = "int pmid", 
-	        methodSummary = "Loads the PubMed entry from a given pmid as XML"
-	        	
-	    )
-	public int loadPubMedEntryInBrowser(int pmid)
-    throws IOException, BioclipseException, CoreException;
-	  
-	  @Recorded
-	  @PublishedMethod(
-	        params = "String pubmedQuery", 
-	        methodSummary = "Returns 20 pubmed Identifiers returned on a pubmed query"
-	        	
-	    )
-    public String findPubmedIdentifiers(String pubmedQuery)
-	throws IOException, BioclipseException, CoreException;
-	  
-	  @Recorded
-	  @PublishedMethod(
-	        params = "String pubmedQuery, int retMax", 
-	        methodSummary = "Returns retMax pubmed Identifiers  a pubmed query."
-	        	
-	    )
-    public String findPubmedIdentifiers(String pubmedQuery, int retMax)
-	throws IOException, BioclipseException, CoreException;
-	  
-	  @Recorded
-	  @PublishedMethod(
-	        params = "String pubmedQuery, int retMax, int retStart", 
-	        methodSummary = "Returns retMax pubmed Identifiers, starting at hit retStart returned on a pubmed query"
-	        	
-	    )
-    public String findPubmedIdentifiers(String pubmedQuery, int retMax, int retStart)
 	throws IOException, BioclipseException, CoreException;
 
-	  @Recorded
-	  @PublishedMethod(
-	        params = "int pmid", 
-	        methodSummary = "Gets the PubMed abstract from a given pmid."
-	        	
-	    )
+
+	@Recorded
+	@PublishedMethod(
+			params = "int pmid", 
+			methodSummary = "Get full text link if available"
+
+	)
+	public String getFullTextLink(int pmid)
+	throws IOException, BioclipseException, CoreException;
+
+	@Recorded
+	@PublishedMethod(
+			params = "int pmid", 
+			methodSummary = "Loads the PubMed entry from a given pmid as XML"
+
+	)
+	public int loadPubMedEntryInBrowser(int pmid)
+	throws IOException, BioclipseException, CoreException;
+
+	@Recorded
+	@PublishedMethod(
+			params = "String pubmedQuery", 
+			methodSummary = "Returns 20 pubmed Identifiers returned on a pubmed query"
+
+	)
+	public String findPubmedIdentifiers(String pubmedQuery)
+	throws IOException, BioclipseException, CoreException;
+
+	@Recorded
+	@PublishedMethod(
+			params = "String pubmedQuery, int retMax", 
+			methodSummary = "Returns retMax pubmed Identifiers  a pubmed query."
+
+	)
+	public String findPubmedIdentifiers(String pubmedQuery, int retMax)
+	throws IOException, BioclipseException, CoreException;
+
+	@Recorded
+	@PublishedMethod(
+			params = "String pubmedQuery, int retMax, int retStart", 
+			methodSummary = "Returns retMax pubmed Identifiers, starting at hit retStart returned on a pubmed query"
+
+	)
+	public String findPubmedIdentifiers(String pubmedQuery, int retMax, int retStart)
+	throws IOException, BioclipseException, CoreException;
+
+	@Recorded
+	@PublishedMethod(
+			params = "int pmid", 
+			methodSummary = "Gets the PubMed abstract from a given pmid."
+
+	)
 	public String getPubMedAbstract(int pmid)
-    throws IOException, BioclipseException, CoreException;
-	  
-	  
-	  @Recorded
-	  @PublishedMethod(
-	        params = "int pmid", 
-	        methodSummary = "Returns the online resource where the pmid is cited through http://www.citedin.org"
-	        	
-	    )
+	throws IOException, BioclipseException, CoreException;
+
+
+	@Recorded
+	@PublishedMethod(
+			params = "int pmid", 
+			methodSummary = "Returns the online resource where the pmid is cited through http://www.citedin.org"
+
+	)
 	public String citedIn(int pmid)
-    throws IOException, BioclipseException, CoreException;
-	  
-	  @Recorded
-	  @PublishedMethod(
-	        params = "int pmid", 
-	        methodSummary = "Returns the online resource where the pmid is cited through http://www.citedin.org"
-	        	
-	    )
+	throws IOException, BioclipseException, CoreException;
+
+	@Recorded
+	@PublishedMethod(
+			params = "int pmid", 
+			methodSummary = "Returns the online resource where the pmid is cited through http://www.citedin.org"
+
+	)
 	public String availableFormats(int pmid)
-    throws IOException, BioclipseException, CoreException;
+	throws IOException, BioclipseException, CoreException;
 }
--- plugins/net.bioclipse.pubmed/src/net/bioclipse/pubmed/business/PubmedManager.java
+++ plugins/net.bioclipse.pubmed/src/net/bioclipse/pubmed/business/PubmedManager.java
@@ -39,30 +39,30 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 
- 
+
 
 public class PubmedManager implements IBioclipseManager {
 
-    private static final Logger logger = Logger.getLogger(PubmedManager.class);
+	private static final Logger logger = Logger.getLogger(PubmedManager.class);
 
-    /**
-     * Gives a short one word name of the manager used as variable name when
-     * scripting.
-     */
-    public String getManagerName() {
-        return "pubmed";
-    }
-    
-    public Document getPubmedEntryasDocument(int pmid) throws ParserConfigurationException, SAXException, IOException{
+	/**
+	 * Gives a short one word name of the manager used as variable name when
+	 * scripting.
+	 */
+	public String getManagerName() {
+		return "pubmed";
+	}
+
+	public Document getPubmedEntryasDocument(int pmid) throws ParserConfigurationException, SAXException, IOException{
 		URL url = new URL("http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id=" + Integer.toString(pmid) + "&retmode=xml");
 		DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
-        Document doc = docBuilder.parse(url.openStream());
-    	return doc;
-    }
-    
-    public String getPubMedEntry(int pmid, IProgressMonitor monitor) throws IOException, BioclipseException, CoreException{
-    	if (monitor == null) {
+		DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+		Document doc = docBuilder.parse(url.openStream());
+		return doc;
+	}
+
+	public String getPubMedEntry(int pmid, IProgressMonitor monitor) throws IOException, BioclipseException, CoreException{
+		if (monitor == null) {
 			monitor = new NullProgressMonitor();
 		}
 
@@ -80,39 +80,39 @@ public class PubmedManager implements IBioclipseManager {
 		while (line != null) {
 			pubmedString = pubmedString + line+'\n';
 			line = reader.readLine();
-			
-		}
-    	
-    	return pubmedString;
-    	
-    }
 
-    
-    public String getPubMedAbstract(int pmid, IProgressMonitor monitor) throws IOException, BioclipseException, CoreException, ParserConfigurationException, SAXException{
-    	if (monitor == null) {
+		}
+
+		return pubmedString;
+
+	}
+
+
+	public String getPubMedAbstract(int pmid, IProgressMonitor monitor) throws IOException, BioclipseException, CoreException, ParserConfigurationException, SAXException{
+		if (monitor == null) {
 			monitor = new NullProgressMonitor();
 		}
 
 		monitor.beginTask(
 				"Downloading PMID: " + pmid + " from PubMed (Eutils).", 2
 		);
-        Document doc = this.getPubmedEntryasDocument(pmid);
-        NodeList pubmedAbstractList = doc.getElementsByTagName("AbstractText");
-        String pubmedAbstract = "";
-        if (pubmedAbstractList.getLength() > 0){
-        
-        for (int i=0; i<pubmedAbstractList.getLength(); i++){
-        	pubmedAbstract = pubmedAbstract + pubmedAbstractList.item(i).getTextContent();
-        }
-        }
-    	
-    	return pubmedAbstract;
-    	
-    }
-    
-    
-    public String getFullTextLink(int pmid, IProgressMonitor monitor) throws IOException, BioclipseException, CoreException, ParserConfigurationException, SAXException{
-    	if (monitor == null) {
+		Document doc = this.getPubmedEntryasDocument(pmid);
+		NodeList pubmedAbstractList = doc.getElementsByTagName("AbstractText");
+		String pubmedAbstract = "";
+		if (pubmedAbstractList.getLength() > 0){
+
+			for (int i=0; i<pubmedAbstractList.getLength(); i++){
+				pubmedAbstract = pubmedAbstract + pubmedAbstractList.item(i).getTextContent();
+			}
+		}
+
+		return pubmedAbstract;
+
+	}
+
+
+	public String getFullTextLink(int pmid, IProgressMonitor monitor) throws IOException, BioclipseException, CoreException, ParserConfigurationException, SAXException{
+		if (monitor == null) {
 			monitor = new NullProgressMonitor();
 		}
 
@@ -120,33 +120,33 @@ public class PubmedManager implements IBioclipseManager {
 				"Searching full text link " + pmid + " from PubMed (Eutils).", 2
 		);
 		URL url = new URL("http://www.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?dbfrom=pubmed&id=" + Integer.toString(pmid) + "&retmode=xml&cmd=prlinks");
-		
+
 
 		DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
-        Document doc = docBuilder.parse(url.openStream());
-        NodeList urlList = doc.getElementsByTagName("Url");
-        String returnUrl = urlList.item(0).getTextContent();
-    	return returnUrl;
-    	
-    }
-    
-    
-    public int loadPubMedEntryInBrowser(int pmid, IProgressMonitor monitor) throws IOException, BioclipseException, CoreException{
+		DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+		Document doc = docBuilder.parse(url.openStream());
+		NodeList urlList = doc.getElementsByTagName("Url");
+		String returnUrl = urlList.item(0).getTextContent();
+		return returnUrl;
 
-        IWebBrowser browser = PlatformUI.getWorkbench().getBrowserSupport().createBrowser(IWorkbenchBrowserSupport.AS_EDITOR, null,"Expert Viewer", "Validation");
-        browser.openURL(new URL("http://www.ncbi.nlm.nih.gov/pubmed/"+pmid));
+	}
 
 
-    	return 0;
-    	
-    }
-    
-    
-    public String findPubmedIdentifiers(String pubmedQuery, int retMax, int retStart, IProgressMonitor monitor) throws IOException, BioclipseException, CoreException{
-   
-    	
-    	if (monitor == null) {
+	public int loadPubMedEntryInBrowser(int pmid, IProgressMonitor monitor) throws IOException, BioclipseException, CoreException{
+
+		IWebBrowser browser = PlatformUI.getWorkbench().getBrowserSupport().createBrowser(IWorkbenchBrowserSupport.AS_EDITOR, null,"Expert Viewer", "Validation");
+		browser.openURL(new URL("http://www.ncbi.nlm.nih.gov/pubmed/"+pmid));
+
+
+		return 0;
+
+	}
+
+
+	public String findPubmedIdentifiers(String pubmedQuery, int retMax, int retStart, IProgressMonitor monitor) throws IOException, BioclipseException, CoreException{
+
+
+		if (monitor == null) {
 			monitor = new NullProgressMonitor();
 		}
 
@@ -164,75 +164,75 @@ public class PubmedManager implements IBioclipseManager {
 		while (line != null) {
 			pubmedString = pubmedString + line+'\n';
 			line = reader.readLine();
-			
+
 		}
-    	
-    	return pubmedString;
-    	
-    }
-    
-    public String findPubmedIdentifiers(String pubmedQuery, int retMax, IProgressMonitor monitor) throws IOException, BioclipseException, CoreException{
-    	   
-    	
-        String pubmedString = this.findPubmedIdentifiers(pubmedQuery, retMax, 0, monitor);
-     	
-     	return pubmedString;
-     	
-     }
-    
- public String findPubmedIdentifiers(String pubmedQuery, IProgressMonitor monitor) throws IOException, BioclipseException, CoreException{
-   
-    	
-       String pubmedString = this.findPubmedIdentifiers(pubmedQuery, 20, monitor);
-    	
-    	return pubmedString;
-    	
-    }
-    
- public String citedIn(int pmid, IProgressMonitor monitor) throws IOException, BioclipseException, CoreException, ParserConfigurationException, SAXException{
-	 if (monitor == null) {
+
+		return pubmedString;
+
+	}
+
+	public String findPubmedIdentifiers(String pubmedQuery, int retMax, IProgressMonitor monitor) throws IOException, BioclipseException, CoreException{
+
+
+		String pubmedString = this.findPubmedIdentifiers(pubmedQuery, retMax, 0, monitor);
+
+		return pubmedString;
+
+	}
+
+	public String findPubmedIdentifiers(String pubmedQuery, IProgressMonitor monitor) throws IOException, BioclipseException, CoreException{
+
+
+		String pubmedString = this.findPubmedIdentifiers(pubmedQuery, 20, monitor);
+
+		return pubmedString;
+
+	}
+
+	public String citedIn(int pmid, IProgressMonitor monitor) throws IOException, BioclipseException, CoreException, ParserConfigurationException, SAXException{
+		if (monitor == null) {
 			monitor = new NullProgressMonitor();
 		}
 
 		monitor.beginTask(
 				"Contacting citedin.org for citation to "+pmid, 2
 		);   
- 	URL url = new URL("http://www.citedin.org/webservice.php/citations?pmid="+pmid);
- 	
- 	BufferedReader reader = new BufferedReader(
-			new InputStreamReader(
-					url.openConnection().getInputStream()
-			)
-	);
-	String line = reader.readLine();
-	String citedInString = ""; 
-	while (line != null || line != "") {
-		citedInString = citedInString + line+'\n';
-		line = reader.readLine();	
+		URL url = new URL("http://www.citedin.org/webservice.php/citations?pmid="+pmid);
+
+		BufferedReader reader = new BufferedReader(
+				new InputStreamReader(
+						url.openConnection().getInputStream()
+				)
+		);
+		String line = reader.readLine();
+		String citedInString = ""; 
+		while (line != null || line != "") {
+			citedInString = citedInString + line+'\n';
+			line = reader.readLine();	
+		}
+
+		DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+		Document doc = docBuilder.parse(citedInString);
+		NodeList citingResources = doc.getChildNodes();
+		String resultCitingResources = "";
+		for (int i=0; i<citingResources.getLength(); i++){
+			if (Integer.parseInt(citingResources.item(i).getFirstChild().getTextContent()) > 0 )
+			{
+				resultCitingResources = resultCitingResources+citingResources.item(i).getNodeName();
+			}
+
+		}
+
+
+
+
+		return resultCitingResources;
+
 	}
-	
- 	DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
-    DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
-    Document doc = docBuilder.parse(citedInString);
-    NodeList citingResources = doc.getChildNodes();
-    String resultCitingResources = "";
-    for (int i=0; i<citingResources.getLength(); i++){
-       if (Integer.parseInt(citingResources.item(i).getFirstChild().getTextContent()) > 0 )
-       {
-    	   resultCitingResources = resultCitingResources+citingResources.item(i).getNodeName();
-       }
-    	
-    }
- 	
- 	
-    
-  	
-  	return resultCitingResources;
-  	
-  }
- 
- public boolean openAccess(int pmid, String journal, IProgressMonitor monitor) throws ParserConfigurationException, SAXException, IOException{
-	 if (monitor == null) {
+
+	public boolean openAccess(int pmid, String journal, IProgressMonitor monitor) throws ParserConfigurationException, SAXException, IOException{
+		if (monitor == null) {
 			monitor = new NullProgressMonitor();
 		}
 
@@ -248,20 +248,20 @@ public class PubmedManager implements IBioclipseManager {
 		boolean openAccess = false;
 		String line = reader.readLine();
 		while (line != null) {
-            if (line.contains("Found 1 journals matching your query"))
-            {
-            	openAccess = true;
-            	break;
-            }
+			if (line.contains("Found 1 journals matching your query"))
+			{
+				openAccess = true;
+				break;
+			}
 			line = reader.readLine();
-			
+
 		}
-   
-	 return openAccess; 
- }
- 
- public String availableFormats(int pmid, IProgressMonitor monitor) throws IOException, BioclipseException, CoreException, ParserConfigurationException, SAXException{
-	 if (monitor == null) {
+
+		return openAccess; 
+	}
+
+	public String availableFormats(int pmid, IProgressMonitor monitor) throws IOException, BioclipseException, CoreException, ParserConfigurationException, SAXException{
+		if (monitor == null) {
 			monitor = new NullProgressMonitor();
 		}
 
@@ -270,25 +270,25 @@ public class PubmedManager implements IBioclipseManager {
 		); 
 		String availableFormatsString = "";
 		Document doc = this.getPubmedEntryasDocument(pmid);
-	    //See if this pubmed entry contains an abstract
-	    if (doc.getElementsByTagName("Abstract").getLength() > 0) {
-	    	availableFormatsString = availableFormatsString+"Pubmed abstract\n";
-	    }
-	    
-	    //See if a link to full text is available
-	    String fullTextLink = this.getFullTextLink(pmid, monitor);
-	    if (fullTextLink != ""){
-	    	availableFormatsString = availableFormatsString + "Full text at: "+ fullTextLink+"\n";
-	    }
-	    
-	    //See if it is available as openAccess
-	    NodeList journalNode = doc.getElementsByTagName("Journal"); 
-	    Element journalElement = (Element) journalNode.item(0);
-	    NodeList titleNode = journalElement.getElementsByTagName("Title");
-	   String journal = titleNode.item(0).getTextContent();
-	    if (this.openAccess(pmid, journal, monitor)){
-	    	availableFormatsString = availableFormatsString + "Open Access\n";
-	    }
+		//See if this pubmed entry contains an abstract
+		if (doc.getElementsByTagName("Abstract").getLength() > 0) {
+			availableFormatsString = availableFormatsString+"Pubmed abstract\n";
+		}
+
+		//See if a link to full text is available
+		String fullTextLink = this.getFullTextLink(pmid, monitor);
+		if (fullTextLink != ""){
+			availableFormatsString = availableFormatsString + "Full text at: "+ fullTextLink+"\n";
+		}
+
+		//See if it is available as openAccess
+		NodeList journalNode = doc.getElementsByTagName("Journal"); 
+		Element journalElement = (Element) journalNode.item(0);
+		NodeList titleNode = journalElement.getElementsByTagName("Title");
+		String journal = titleNode.item(0).getTextContent();
+		if (this.openAccess(pmid, journal, monitor)){
+			availableFormatsString = availableFormatsString + "Open Access\n";
+		}
 		return availableFormatsString;
- }
+	}
 }
